6184cf71992c64412cce01fb78e7e76d48d59e2e
Alejandro Costa <alejandroesfc@gmail.com>
2022-04-11 06:00:15 -0300

    Profile .matches returns true when there is a match. Refactoring in the tests with addition of @BeforeEach methods

--- src/iloveyouboss/Profile.java
+++ src/iloveyouboss/Profile.java
@@ -1,10 +1,16 @@
 package iloveyouboss;
 
 public class Profile {
+	
+	private Answer answer;
+	
+	public void add(Answer answer) {
+		this.answer = answer;
+	}
 
 	public boolean matches(Criterion criterion) {
 		
-		return false;
+		return answer != null;
 		
 	}
 
--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -1,21 +1,48 @@
 package iloveyouboss;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProfileTest {
+	
+	private Profile profile;
+	private BooleanQuestion questionIsThereRelocation;
+	private Answer answerThereIsRelocation;
+	
+	@BeforeEach
+	public void createProfile() {
+		this.profile = new Profile();
+	}
+	
+	@BeforeEach
+	public void createQuestionAndAnswer() {
+		this.questionIsThereRelocation = new BooleanQuestion(1, "Relocation pacakge?");
+		this.answerThereIsRelocation = new Answer(questionIsThereRelocation, Bool.TRUE);
+	}
 
 	@Test
 	public void matchesNothingWhenProfileEmpty() {
 		
-		Profile profile = new Profile();
-		Question question = new BooleanQuestion(1, "Relocation package?");
-		Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare);
+		Criterion criterion = new Criterion(answerThereIsRelocation, Weight.DontCare);
 		
 		boolean result = profile.matches(criterion);
 		
 		assertFalse(result);
+		
+	}
+	
+	@Test
+	public void matchesWhenProfileContainsMatchingAnswer() {
+		
+		profile.add(answerThereIsRelocation);
+		Criterion criterion = new Criterion(new Answer(questionIsThereRelocation, Bool.TRUE), Weight.Important);
+		
+		boolean result = profile.matches(criterion);
+		
+		assertTrue(result);
 		
 	}
 
